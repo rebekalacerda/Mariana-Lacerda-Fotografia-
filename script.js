f203f514831bb95abc88d5bc629065cdd42eea4d
Rebeka L Paes <133894733+rebekalacerda@users.noreply.github.com>
2026-07-21 19:45:13 -0300

    Add files via upload
    Arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById("menu");
+const botao = document.getElementById("menu-btn");
+
+botao.addEventListener("click", () => {
+
+    menu.classList.toggle("active");
+
+});
